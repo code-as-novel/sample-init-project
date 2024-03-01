b2fd8b57aa4dda0f5b10184aca8fa1b293fed842
Soo <fgump78@gmail.com>
2024-03-01 14:21:37 +0900

    first commit

---
+++ src/main/java/com/codeasnovel/init/InitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codeasnovel.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitProjectApplication.class, args);
+	}
+
+}
